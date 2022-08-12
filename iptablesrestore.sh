319dc8db6d54f84c87c7d0f233eaf244ca8a6c12
bsvnix <111151951+bsvnix@users.noreply.github.com>
2022-08-12 15:23:26 -0600

    Create iptablesrestore.sh
    Adding posability to route between WWAN ant ETH on Tektelic Kona GW

---
+++ iptablesrestore.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+EXTIF=wwan0
+INTIF=eth0
+iptables -A FORWARD -i $EXTIF -o $INTIF -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -i $INTIF -o $EXTIF -j ACCEPT
+iptables -t nat -A POSTROUTING -o $EXTIF -j MASQUERADE
