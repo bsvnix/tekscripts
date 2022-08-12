c684649bb038267b2a62d66f57b1f36f61a3c301
bsvnix <111151951+bsvnix@users.noreply.github.com>
2022-08-12 15:45:30 -0600

    Update iptablesrestore.sh

--- iptablesrestore.sh
+++ iptablesrestore.sh
@@ -1,6 +1,7 @@
-#! /bin/bash
+!# /bin/bash
 EXTIF=wwan0
 INTIF=eth0
+echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -A FORWARD -i $EXTIF -o $INTIF -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -i $INTIF -o $EXTIF -j ACCEPT
 iptables -t nat -A POSTROUTING -o $EXTIF -j MASQUERADE
